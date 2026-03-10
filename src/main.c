268839c608d5e21ac91f658368f9123e08458900
Ksenia-Avdeevich <avdeevich.ksenia@gmail.com>
2026-03-10 11:54:23 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,46 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include "group.h"
 #include "person.h"
 
-#define NUM_PERSONS 3
-
 int main(void) {
-    /* Задача 3: структура GROUP */
-    int n;
-    printf("GROUP structure demo\n");
-    printf("Enter number of students (1-%d): ", MAX_STUDENTS);
-    scanf("%d", &n);
-    if (n < 1 || n > MAX_STUDENTS) n = MAX_STUDENTS;
+    /* структура GROUP */
+    printf("GROUP structure demo \n");
 
-    GROUP gr5[MAX_STUDENTS];
-    for (int i = 0; i < n; i++) input_student(&gr5[i], i);
+    GROUP gr5[MAX_STUDENTS] = {
+        {"Ivanov I.I.",   {1993, 3, 15}, {4, 5, 4}},
+        {"Petrov P.P.",   {1991, 6, 20}, {3, 3, 4}},
+        {"Sidorov S.S.",  {1990, 11, 1}, {5, 4, 5}},
+        {"Kozlov K.K.",   {1992, 8, 10}, {3, 4, 3}},
+        {"Morozov M.M.",  {1994, 2, 28}, {4, 4, 4}},
+        {"Sokolov S.S.",  {1989, 5, 5},  {5, 5, 5}},
+        {"Novikov N.N.",  {1995, 7, 14}, {3, 3, 3}},
+        {"Volkov V.V.",   {1988, 12, 31},{4, 5, 4}},
+        {"Lebedev L.L.",  {1996, 1, 22}, {3, 4, 4}},
+        {"Fedorov F.F.",  {1987, 9, 3},  {5, 4, 5}},
+    };
+    int n = MAX_STUDENTS;
 
-    printf("\nSorted by GPA (ascending)\n");
+    printf("\n--- All students ---\n");
+    for (int i = 0; i < n; i++) print_student(&gr5[i]);
+
+    printf("\n--- Sorted by GPA (ascending) ---\n");
     sort_by_ses(gr5, n);
     for (int i = 0; i < n; i++) print_student(&gr5[i]);
 
-    printf("\nStudents under 20 on 01.12.2010\n");
+    printf("\n--- Students under 20 on 01.12.2010 ---\n");
     print_young_students(gr5, n);
 
-    /* Задача 4: структура «Человек»*/
+    /* структура «Человек»*/
     printf("\n=== PERSON structure demo ===\n");
-    Person people[NUM_PERSONS] = {
-        {"Ivanov",  "Ivan",  "Ivanovich",  "Male",   "Russian", 178, 75,
-         {1990, 5, 15}, "+7-900-000-0001",
-         {"220000", "Belarus", "Minsk region", "Minsk district",
-          "Minsk", "Lenina", 1, 10}},
-        {"Petrov",  "Petr",  "Petrovich",  "Male",   "Russian", 182, 80,
-         {2002, 3, 22}, "+7-900-000-0002",
-         {"220001", "Belarus", "Minsk region", "Minsk district",
-          "Minsk", "Mira", 5, 20}},
-        {"Sidorova","Anna",  "Ivanovna",   "Female", "Russian", 165, 55,
-         {1998, 11, 1}, "+7-900-000-0003",
-         {"220002", "Belarus", "Minsk region", "Minsk district",
-          "Minsk", "Pobedy", 3, 7}},
-    };
 
-    const Person *youngest = find_youngest(people, NUM_PERSONS);
+    Person people[] = {
+        {
+            "Ivanov", "Ivan", "Ivanovich",
+            "Male", "Russian", 178.0f, 75.0f,
+            {1990, 5, 15}, "+375-29-000-00-01",
+            {"220000", "Belarus", "Minsk region", "Minsk district",
+             "Minsk", "Lenina", 1, 10}
+        },
+        {
+            "Petrov", "Petr", "Petrovich",
+            "Male", "Russian", 182.0f, 80.0f,
+            {2002, 3, 22}, "+375-29-000-00-02",
+            {"220001", "Belarus", "Minsk region", "Minsk district",
+             "Minsk", "Mira", 5, 20}
+        },
+        {
+            "Sidorova", "Anna", "Ivanovna",
+            "Female", "Belarusian", 165.0f, 55.0f,
+            {1998, 11, 1}, "+375-29-000-00-03",
+            {"220002", "Belarus", "Grodno region", "Grodno district",
+             "Grodno", "Pobedy", 3, 7}
+        },
+    };
+    int np = sizeof(people) / sizeof(people[0]);
+
+    const Person *youngest = find_youngest(people, np);
     printf("Youngest person:\n");
     if (youngest) print_person(youngest);
 
